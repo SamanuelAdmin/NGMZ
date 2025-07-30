a5b2d2d9a303c03a9b9a6cbac0474bb46a378bef
mark <Samanuel Admin>
2025-07-30 18:17:07 +0000

    requirements.txt update

--- NGMZ2/settings.py
+++ NGMZ2/settings.py
@@ -110,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/' if DEBUG else '/staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
